4d2f0adeb63d80856a127d57e240625fc889191a
Данил А. Терновых <ternovikh@yamoney.ru>
2018-03-28 13:05:24 +0300

    correct shift decoration

--- app/src/main/java/yamoney/ru/customlayoutmanager/ShiftDecoration.kt
+++ app/src/main/java/yamoney/ru/customlayoutmanager/ShiftDecoration.kt
@@ -7,9 +7,9 @@ import android.view.View
 class ShiftDecoration(private val shift: Int) : RecyclerView.ItemDecoration() {
 
     override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
-        val position = parent.getChildAdapterPosition(view)
-        val left = shift * (position % 2)
-        val right = shift * ((position + 1) % 2)
+        val group = parent.getChildAdapterPosition(view) / 2
+        val left = shift * (group % 2)
+        val right = shift * ((group + 1) % 2)
         outRect.set(left, 0, right, 0)
     }
 }
